891ed73f3dcbedd2bd7e48fc2b3eb6d550747fc2
ushio0107 <ushio000107@gmail.com>
2023-08-22 11:50:53 +0800

    fix: update db url

--- db/db.go
+++ db/db.go
@@ -12,7 +12,7 @@ import (
 )
 
 func NewDB() (*mongo.Collection, error) {
-	uri := fmt.Sprintf("mongodb://%v:%v@%v:%v/Management?authSource=admin", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
+	uri := fmt.Sprintf("mongodb://%v:%v@%v:%v", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
 	log.Print("Connecting to DB: ", uri)
 
 	clientOptions := options.Client().ApplyURI(uri)
